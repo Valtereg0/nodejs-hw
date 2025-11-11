e59497b01ce11a94ed188c29a22298fe8b3d198f
Valerii Riznyck <valera451reznick@gmail.com>
2025-11-11 20:13:58 +0200

    fix

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-import express from 'express';
 import 'dotenv/config';
+import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import { connectMongoDB } from './db/connectMongoDB.js';
