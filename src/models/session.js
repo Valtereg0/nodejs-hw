554e272f2b74d53155164c759925c6e105b7e046
Valerii Riznyck <valera451reznick@gmail.com>
2025-11-26 13:10:57 +0200

    fix authenticate file

--- src/models/session.js
+++ src/models/session.js
@@ -9,7 +9,7 @@ const sessionSchema = new Schema(
     refreshTokenValidUntil: { type: Date, required: true },
   },
   {
-    timestamps: true,
+    timestamps: true, versionKey: false,
   }
 );
 
--- src/models/user.js
+++ src/models/user.js
@@ -6,7 +6,7 @@ const userSchema = new Schema({
   password: { type: String, required: true },
 },
   {
-    timestamps: true,
+    timestamps: true, versionKey: false,
   }
 );
 
