23d75a93a8d5468adf25e49fea6651802d00cdc8
Valerii Riznyck <valera451reznick@gmail.com>
2025-12-02 21:36:53 +0200

    fix multer

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { updateUserAvatar } from "../controllers/userController.js";
 import { authenticate } from "../middleware/authenticate.js";
-import { upload } from "../upload/multer.js";
+import { upload } from "../middleware/multer.js";
 
 const router = Router();
 
